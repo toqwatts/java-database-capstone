35a6661688bc2e62f6ddb7dcd451ec2b754825bc
James Watts <toq_99@yahoo.com>
2025-10-14 20:08:18 -0500

    Revert "updated files"
    This reverts commit b84cb288397314ca2f38e7084a2bdeaa8468391a.

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -37,7 +37,7 @@ public class Admin {
     public void setUsername(String username) {
         this.username = username;
     }
-    public void setPassword(String password) {
+    public String setPassword(String password) {
         this.password = password;
     }
 }
